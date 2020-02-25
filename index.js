07e2b2a03cd8e30b546cc072ac70bba697bb748c
Diego Louli <diego.louli@gmail.com>
2020-02-25 10:54:16 +0100

    fixup! fixup! fixup! fixup! Fix invalid variable

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const { Toolkit } = require('actions-toolkit');
 
 Toolkit.run(
     async tools => {
-        const { repository } = tools.context.payload;
+        const repository = tools.context.payload.repository;
+        const pull_request = tools.context.payload.pull_request;
         tools.log(tools.context);
         const commitMessageRegex = core.getInput('commitMessageRegex');
 
         const params = {
             owner: repository.owner.login,
             repo: repository.name,
-            pull_number: repository.pull_request.number
+            pull_number: pull_request.number
         }
 
         const commits = (await tools.github.pulls.listCommits(params)).data;
